c090fdf4916ed60770a5862c994dc214721ebb9e
Witold Frącek <witek.fracek@gmail.com>
2024-01-14 13:30:53 +0100

    better stops loading

--- load_data/load_data.py
+++ load_data/load_data.py
@@ -1,200 +1,223 @@
-import pandas as pd
 import os
-from bs4 import BeautifulSoup
-from tqdm import tqdm
+import random
+
+import pandas as pd
 import networkx as nx
+
+from bs4 import BeautifulSoup, ResultSet
+from tqdm import tqdm
 from stops_utils import is_bus_line
-from typing import Literal
+from typing import Literal, Union
+
+def __random_color() -> str:
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return f'#{r:02X}{g:02X}{b:02X}'
+
+
+def get_line_colors(graph: nx.DiGraph) -> list[str]:
+    lines = set(map(lambda stop: stop.line, graph.nodes))
+    color_map = {line: __random_color() for line in lines}
+    return [color_map[stop.line] for stop in graph.nodes]
 
 
 class Stop:
 
-    __slots__ = ['name', 'code', 'lat', 'lon']
+    __slots__ = ['name', 'code', 'lat', 'lon', 'line']
 
-    def __init__(self, name: str, code: int, latitude: float, longitude: float):
+    def __init__(self, name: str, code: int, latitude: float, longitude: float, line: str):
         self.name = name
         self.code = code
         self.lat = latitude
         self.lon = longitude
+        self.line = line
     
     def __repr__(self) -> str:
-        return f'Stop: "{self.name}"'
+        return f'line {self.line}: "{self.name}"'
     
-    def __eq__(self, other) -> bool:
-        return self.name == other.name
+    def __eq__(self, other: Union[str, "Stop"]) -> bool:
+        if isinstance(other, str):
+            return self.name == other
+        if isinstance(other, Stop):
+            return self.name == other.name and self.line == other.line
+        return False
     
     def __hash__(self):
-        return hash(self.name)
+        return hash(self.name + self.line)
 
     def __lt__(self, other):
         return self.name <= other.name
 
 
-def load_stops(path: str, strategy: Literal['first', 'mean']='mean') -> pd.DataFrame:
-    """Function that returns DataFrame with names and geolocalisation for bus and tram stops.
-
-    Args:
-        path (str): path to the file with stops data.
-        strategy (Optional[str]): strategy used when a stop has more than one pair
-        of coordinates. Available options: 'first', 'mean'. Defaults to 'first'.
+class MPKGraphLoader:
+    def __init__(self, data_path: str, transfer_time: float = 5.0) -> None:
+        self.__data_path = data_path
+        self.__stops_data = self.__load_stops_df()
+        routes = self.__load_routes()
+        self.__line_graphs: dict[str, nx.DiGraph] = {}
+        for line_name, routes_data in routes.items():
+            line_graph = self.__get_line_graph(*routes_data)
+            self.__line_graphs[line_name] = line_graph
+        
+        self.__total_graph = self.__get_total_graph(transfer_time)
     
-    Returns: (DataFrame)
-
-    Columns description:
-        stop_name: name of the stop
-        stop_lat: latitude of the stop
-        stop_lon: longitude of the stop
-        stop_code: unique code of the stop
-    """
-    stops_df = pd.read_csv(path)
-    stops_df = stops_df[['stop_name', 'stop_lat', 'stop_lon', 'stop_code']]
-    stops_df['stop_name'] = stops_df['stop_name'].str.lower()
-    coords = stops_df[['stop_name', 'stop_lat', 'stop_lon']]
-    codes = stops_df[['stop_name', 'stop_code']]
-    codes = codes.groupby(['stop_name'], as_index=False).first()
-    if strategy == 'mean':
-        coords = coords.groupby(['stop_name'], as_index=False).mean()
-    else:
-        coords = coords.groupby(['stop_name'], as_index=False).first()
-    data = coords.join(codes.set_index('stop_name'), on='stop_name')
-    return data
-
-
-RouteData = dict[str, tuple[list[Stop], list[Stop]]]
-TimeData = dict[str, tuple[list[int], list[int]]]
-
-
-def load_routes(path: str, stops: pd.DataFrame) -> tuple[RouteData, TimeData]:
-    """Function that loads all routes data. Returns a dict which keys (str) are the names of the lines,
-    and the values are 2 element tuples where first element is the list of stops (list[Stop]) for the first variant of the
-    route, and the second element is the list of stops for the second vartiant of the route.
-
-    Args:
-        path (str): path to the directory that stores all directories with xml files.
-        stops (DataFrame): dataframe that contains information about stops names, ids, latitudes and longitudes.
+    @property
+    def total_graph(self) -> nx.DiGraph:
+        return self.__total_graph
     
-    Returns: (dict[str, tuple[list[Stop], list[Stop]]])
-    """
-    ret = {}
-    route_times = {}
-    xml_paths = __get_xml_paths(path)
-    for path in tqdm(xml_paths):
-        line_name, var1, var2 = __load_line_variants_from_xml(path)
-        stop_list_var1, route_times1 = __get_stop_list(var1, stops)
-        stop_list_var2, route_times2 = __get_stop_list(var2, stops)
-        ret[line_name] = (stop_list_var1, stop_list_var2)
-        route_times[line_name] = (route_times1, route_times2)
-    return ret, route_times
+    @property
+    def tram_line_names(self) -> list[str]:
+        return list(filter(lambda x: not self.__is_bus_line(x), self.__line_graphs.keys()))
+    
+    @property
+    def bus_line_names(self) -> list[str]:
+        return list(filter(self.__is_bus_line, self.__line_graphs.keys()))
+    
+    def __getitem__(self, line_name: str) -> nx.DiGraph:
+        return self.__line_graphs[line_name]
 
-
-def __get_xml_paths(path: str) -> list:
-    xmls = []
-    for filename in os.listdir(path):
-        full_path = os.path.join(path, filename, f'{filename}.xml')
-        xmls.append(full_path)
-    return xmls
-
-
-def __load_line_variants_from_xml(xml_path: str) -> tuple:
-    with open(xml_path, 'r', encoding='utf-8') as file:
-        data = file.read()
-    data = BeautifulSoup(data, 'xml')
-    name = data.find('linie').find('linia').get('nazwa').lower()
-    stops1_xml = data.find('linie').find('wariant', {'id': 1}).find('czasy').find_all('przystanek')
-    stops2_xml = data.find('linie').find('wariant', {'id': 2}).find('czasy').find_all('przystanek')
-    return name, stops1_xml, stops2_xml
-
-
-def __get_stop_list(stops_xml: list, stops: pd.DataFrame) -> tuple[list[Stop], list[int]]:
-    ret = []
-    times = []
-    errs_count = 0
-    delta_time = 0
-    for i, stop in enumerate(stops_xml):
-        name = stop.get('nazwa').lower()
-        code = int(stop.get('id'))
-        time = 0
-        if i != 0:
-            time = int(stop.get('czas')) - delta_time
-            delta_time += time
+    def to_pickle(self, path: str):
+        import pickle
+        with open(path, 'wb') as file:
+            pickle.dump(self, file)
+    
+    @staticmethod
+    def from_pickle(path: str) -> "MPKGraphLoader":
+        import pickle
+        with open(path, 'rb') as file:
+            return pickle.load(file)
+    
+    def __is_bus_line(self, name: str):
+        if name.isalpha():
+            return True
         try:
-            lat, lon = stops.loc[stops['stop_name'] == name, ['stop_lat', 'stop_lon']].values[0]
-        except IndexError:
-            lat, lon = None, None
-            errs_count += 1
-        s = Stop(name, code, lat, lon)
-        ret.append(s)
-        if i != 0:
-            times.append(time)
-    if errs_count != 0:
-        print(f'WARNING: {errs_count} values without geolocalisation.')
-    return ret, times
-
-
-def load_to_graph(routes: RouteData, times: TimeData) -> nx.Graph:
-    ret = nx.Graph()
-    for name, (r1, r2) in routes.items():
-        t1, t2 = times[name]
-        ret = __add_stop_nodes(ret, r1)
-        ret = __add_stop_nodes(ret, r2)
-        ret = __add_edges(ret, r1, t1)
-        ret = __add_edges(ret, r2, t2)
-    ret.remove_edges_from(nx.selfloop_edges(ret))
-    return ret
-
-
-def get_all_stops(routes: dict) -> set:
-    ret = set()
-    for _, (r1, r2) in routes.items():
-        for stop in r1:
-            ret.add(stop)
-        for stop in r2:
-            ret.add(stop)
-    return ret
-
-
-def get_tram_graph(routes: RouteData, times: TimeData) -> nx.Graph:
-    ret = nx.Graph()
-    for name, (r1, r2) in routes.items():
-        if not is_bus_line(name):
-            ret = __add_stop_nodes(ret, r1)
-            ret = __add_stop_nodes(ret, r2)
-            ret = __add_edges(ret, r1)
-            ret = __add_edges(ret, r2)
-    ret.remove_edges_from(nx.selfloop_edges(ret))
-    return ret
-
-
-def get_bus_graph(routes: RouteData, times: TimeData) -> nx.Graph:
-    ret = nx.Graph()
-    for name, (r1, r2) in routes.items():
-        if is_bus_line(name):
-            ret = __add_stop_nodes(ret, r1)
-            ret = __add_stop_nodes(ret, r2)
-            ret = __add_edges(ret, r1)
-            ret = __add_edges(ret, r2)
-    ret.remove_edges_from(nx.selfloop_edges(ret))
-    return ret
-
-
-def __add_stop_nodes(graph: nx.Graph, route: list[Stop]) -> nx.Graph:
-    ret = nx.Graph(graph)
-    for stop in route:
-        ret.add_node(stop.name, lat=stop.lat, lon=stop.lon)
-    return ret
-
-
-def __add_edges(graph: nx.Graph, route: list[Stop], times: list[int]) -> nx.Graph:
-    ret = nx.Graph(graph)
-    for i in range(len(route) - 1):
-        u: Stop = route[i]
-        v: Stop = route[i + 1]
-        time = times[i]
-        ret.add_edge(u.name, v.name, time=time)
-    return ret
-
-
+            number = int(name)
+        except ValueError:
+            return False
+        if number >= 100:
+            return True
+        return False
+    
+    def get_tram_liens(self) -> dict[str, nx.DiGraph]:
+        ret = {}
+        for name, graph in self.__line_graphs.items():
+            if not self.__is_bus_line(name):
+                ret[name] = graph
+        return ret
+    
+    def get_bus_lines(self) -> dict[str, nx.DiGraph]:
+        ret = {}
+        for name, graph in self.__line_graphs.items():
+            if self.__is_bus_line(name):
+                ret[name] = graph
+        return ret
+    
+    def get_sugraph(self, lines: list[str]) -> nx.DiGraph:
+        ret = nx.DiGraph()
+        for u, v in self.total_graph.edges:
+            if u.line in lines and v.line in lines:
+                ret.add_edge(u, v, time=self.total_graph[u][v])
+        return ret
 
     
+    def __load_stops_df(self) -> pd.DataFrame:
+        df_path = os.path.join(self.__data_path, 'stops.txt')
+        stops_df = pd.read_csv(df_path)
+        stops_df = stops_df[['stop_name', 'stop_lat', 'stop_lon', 'stop_code']]
+        stops_df['stop_name'] = stops_df['stop_name'].str.lower()
+        coords = stops_df[['stop_name', 'stop_lat', 'stop_lon']]
+        codes = stops_df[['stop_name', 'stop_code']]
+        codes = codes.groupby(['stop_name'], as_index=False).first()
+        coords = coords.groupby(['stop_name'], as_index=False).mean()
+        data = coords.join(codes.set_index('stop_name'), on='stop_name')
+        return data
+    
+    def __load_routes(self) -> dict[str, tuple[list[Stop], list[Stop], list[int], list[int]]]:
+        ret = {}
+        xml_paths = self.__get_xml_paths()
+        for path in tqdm(xml_paths):
+            line_name, var1, var2 = self.__load_line_variants_from_xml(path)
+            stop_list_var1, route_times1 = self.__get_stop_list(line_name, var1)
+            stop_list_var2, route_times2 = self.__get_stop_list(line_name, var2)
+            ret[line_name] = (stop_list_var1, stop_list_var2, route_times1, route_times2)
+        return ret
+    
+    def __get_xml_paths(self) -> list[str]:
+        path = os.path.join(self.__data_path, 'lines')
+        xmls = []
+        for filename in os.listdir(path):
+            full_path = os.path.join(path, filename, f'{filename}.xml')
+            xmls.append(full_path)
+        return xmls
+    
+    def __load_line_variants_from_xml(self, xml_path: str) -> tuple[str, ResultSet, ResultSet]:
+        with open(xml_path, 'r', encoding='utf-8') as file:
+            data = file.read()
+        data = BeautifulSoup(data, 'xml')
+        name = data.find('linie').find('linia').get('nazwa').lower()
+        stops1_xml = data.find('linie').find('wariant', {'id': 1}).find('czasy').find_all('przystanek')
+        stops2_xml = data.find('linie').find('wariant', {'id': 2}).find('czasy').find_all('przystanek')
+        return name, stops1_xml, stops2_xml
+    
+    def __get_stop_list(self, line_name: str, stops_xml: list) -> tuple[list[Stop], list[int]]:
+        ret = []
+        times = []
+        errs_count = 0
+        delta_time = 0
+        for i, stop in enumerate(stops_xml):
+            name = stop.get('nazwa').lower()
+            code = int(stop.get('id'))
+            time = 0
+            if i != 0:
+                time = int(stop.get('czas')) - delta_time
+                delta_time += time
+            try:
+                lat, lon = self.__stops_data.loc[self.__stops_data['stop_name'] == name, ['stop_lat', 'stop_lon']].values[0]
+            except IndexError:
+                lat, lon = None, None
+                errs_count += 1
+            s = Stop(name, code, lat, lon, line_name)
+            ret.append(s)
+            if i != 0:
+                times.append(time)
+        if errs_count != 0:
+            print(f'WARNING: {errs_count} values without geolocalisation.')
+        return ret, times
+    
+    def __get_line_graph(self, variant1: list[Stop], variant2: list[Stop], times1: list[int], times2: list[int]) -> nx.DiGraph:
+        ret = nx.DiGraph()
+        ret = self.__get_one_variant(ret, variant1, times1)
+        ret = self.__get_one_variant(ret, variant2, times2)
+        return ret
+        
+    def __get_one_variant(self, graph: nx.DiGraph, variant: list[Stop], times: list[int]) -> nx.DiGraph:
+        for i in range(len(variant) - 1):
+            u: Stop = variant[i]
+            v: Stop = variant[i+1]
+            t: int = times[i]
+            graph.add_node(u)
+            graph.add_node(v)
+            graph.add_edge(u, v, time=t)
+        return graph
+    
+    def __get_total_graph(self, transfer_time: float) -> nx.DiGraph:
+        ret = nx.DiGraph()
+        for line1, line_1_graph in self.__line_graphs.items():
+            for line2, line_2_graph in self.__line_graphs.items():
+                if line1 != line2:
+                    ret.add_nodes_from(line_1_graph.nodes)
+                    ret.add_nodes_from(line_2_graph.nodes)
+                    ret.add_edges_from(line_1_graph.edges)
+                    ret.add_edges_from(line_2_graph.edges)
 
-
+                    line1_stop_names = set(map(lambda stop: stop.name, line_1_graph.nodes))
+                    line2_stop_names = set(map(lambda stop: stop.name, line_2_graph.nodes))
+                    common_names = line1_stop_names & line2_stop_names
+                    for name in common_names:
+                        line1_stop = list(filter(lambda stop: stop == name, line_1_graph.nodes))[0]
+                        line2_stop = list(filter(lambda stop: stop == name, line_2_graph.nodes))[0]
+                        ret.add_edge(line1_stop, line2_stop, time=transfer_time)
+                        ret.add_edge(line2_stop, line1_stop, time=transfer_time)
+        ret.remove_edges_from(nx.selfloop_edges(ret))
+        return ret
